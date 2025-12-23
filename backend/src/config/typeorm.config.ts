f4bab41cca4f8fe68674b715675a8fa55392e1d6
aouguenoune <toufik@tooodooo.com>
2025-12-23 18:22:44 +0100

    `Update typeorm.config.ts and tsconfig.json to handle TypeScript and JavaScript migrations`

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -3,15 +3,21 @@ import { config } from 'dotenv';
 
 config();
 
+const isTs = __filename.endsWith('.ts');
+
 export default new DataSource({
   type: 'mysql',
   host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+  port: parseInt(process.env.DATABASE_PORT ?? '3306', 10),
   username: process.env.DATABASE_USER || 'root',
   password: process.env.DATABASE_PASSWORD || '',
   database: process.env.DATABASE_NAME || 'csps_db',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  migrations: process.env.NODE_ENV === 'development' ? [__dirname + '/../migrations/*{.ts}'] : [__dirname + '/../migrations/*{.js}'],
+  migrations: [
+    isTs
+      ? __dirname + '/../migrations/*.ts'
+      : __dirname + '/../migrations/*.js',
+  ],
   synchronize: false,
   logging: true,
 });
